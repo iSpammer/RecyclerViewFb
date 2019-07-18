82e18bafc668a139e9766dacb5eea5256914e5db
iSpammer <ispamossama@gmail.com>
2019-07-18 22:26:37 +0200

    Initial commit

---
+++ app/src/main/java/ispam/markt17/recyclerviewfb/MainActivity.java
@@ -0,0 +1,72 @@
+package ispam.markt17.recyclerviewfb;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        new GetDataFromFirebase().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+        // Read from the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Places");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                /*for (DataSnapshot alert: dataSnapshot.getChildren()) {
+                    System.out.println(alert.getValue());
+                }*/
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                ArrayList<String> values = (ArrayList<String>) dataSnapshot.getValue();
+                recyclerView.setAdapter(new RecyclerViewAdapter(values));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value." + error.toException());
+            }
+        });
+    }
+
+    private class GetDataFromFirebase extends AsyncTask<Void,Void,Boolean>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+        }
+    }
+}
---
+++ app/src/main/java/ispam/markt17/recyclerviewfb/RecyclerViewAdapter.java
@@ -0,0 +1,44 @@
+package ispam.markt17.recyclerviewfb;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JEFRI SINGH(ஜெப்ரி சிங்) on 11/4/2016.
+ * Organization "The Tuna Group" - Kerala
+ */
+
+class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private ArrayList<String> values;
+    RecyclerViewAdapter(ArrayList<String> values) {
+        this.values = values;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.name.setText(values.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView name;
+        ViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.list_item_text);
+        }
+    }
+}
